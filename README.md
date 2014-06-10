3d9c2a45c0d085a29738f0a5d9de7a0fe5c6cf92
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2014-06-10 13:07:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Typ.js
+
+A simple library for getting any variable's type, with the added ability create
+and map your own custom types if necessary.
+
+There are 2 files, 1 that works within the require.js framework and the other
+that will work anywhere.
+
+Documentation forthcoming (probably not.)
---
+++ typ.js
@@ -0,0 +1,63 @@
+(function(global, undefined){
+
+var toString, toStringMap, toStringRegex, toElementRegex, typ;
+
+if (global.typ) {
+	throw new Error('window.typ already defined');
+}
+
+toString    = Object.prototype.toString;
+toStringMap = {};
+
+toStringRegex  = /(\[object\ |\])/g;
+toElementRegex = /html[\w]*element/;
+
+typ = {
+	of: function(toTest){
+		var type, mType;
+
+		type = toString.call(toTest)
+			.replace(toStringRegex, '')
+			.toLowerCase();
+
+		mType = toStringMap[type];
+
+		if (!mType && type.match(toElementRegex)) {
+			mType = 'element';
+		}
+
+		return mType || type;
+	},
+
+	add: function(base, conversion){
+		var key;
+		if (typ.of(base) === 'string') {
+			toStringMap[base.toLowerCase()] = conversion.toLowerCase();
+		} else if (typ.of(base) === 'object') {
+			for (key in base) {
+				if (base.hasOwnProperty(key)) {
+					toStringMap[key.toLowerCase()] = base[key].toLowerCase();
+				}
+			}
+		}
+
+		return this;
+	},
+
+	remove: function(){
+		var k;
+		for (k = 0; k < arguments.length; k++) {
+			toStringMap[arguments[k]] = undefined;
+		}
+		return this;
+	},
+
+	removeAll: function(){
+		toStringMap = {};
+		return this;
+	}
+};
+
+global.typ = typ;
+
+})(this);
